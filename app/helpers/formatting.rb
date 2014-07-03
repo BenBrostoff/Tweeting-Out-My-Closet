9c542ac20b17a8f92e9f1cfbf119d41849ad4ac5
Ben Brostoff & Greg <pair+benbrostoff.greg99799@devbootcamp.com>
2014-07-03 16:24:32 -0400

    Images cleaned up

--- app/helpers/formatting.rb
+++ app/helpers/formatting.rb
@@ -1,11 +1,13 @@
 helpers do
-  def em(text)
-    "<em>#{text}</em>"
-  end
 
-  def randomize_buttons(a, b, c, d, e)
-    array = [a, b, c, d, e]
-    array.shuffle
-  end
+  require 'digest/md5'
+
+    def em(text)
+      "<em>#{text}</em>"
+    end
+
+    def gravatar_hash(email)
+      Digest::MD5.hexdigest(email.downcase.strip)
+    end
 
 end
