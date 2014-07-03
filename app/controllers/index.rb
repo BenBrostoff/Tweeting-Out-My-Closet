88511c8cd12882fb6bc6781123b72efd9b5e2476
Ben Brostoff & Greg <pair+benbrostoff.greg99799@devbootcamp.com>
2014-07-03 15:33:45 -0400

    Matt fixed config.ru

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,9 +1,25 @@
-get '/' do
+get '/' do #this should be a log-in screen
   erb :index
 end
 
-get '/login' do
+# post '/users' do
+
+# end
+
+# get 'users/:id' do#all your tweets and how many votes you have
+
+# end
+
+# get '/game/:tweet_id' do
+#   tweet_number = Tweet.all.size
+#   @tweet = Tweet.find(rand(tweet_number) + 1) #random tweet
+#   erb :game_page
+
+# end
+
+# post '/game/:tweet_id/votes/:vote_id' do #votes are created in this handler
+
+# end
 
 
-end
 
--- config.ru
+++ config.ru
@@ -2,5 +2,6 @@
 require ::File.expand_path('../config/environment',  __FILE__)
 
 set :app_file, __FILE__
+set :views, Proc.new { File.join(root, "app/views")}
 
 run Sinatra::Application
