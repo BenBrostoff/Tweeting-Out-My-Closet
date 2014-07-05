af569695cf3f3571dd511a5765817f6b8c64b31f
Ben Brostoff & Sid Patel <pair+benbrostoff.sidpatel13@devbootcamp.com>
2014-07-05 15:40:33 -0400

    CSS styling and now working on revising schema to allow for ordering of votes

--- db/migrate/20140703135448_create_tweets.rb
+++ db/migrate/20140703135448_create_tweets.rb
@@ -4,6 +4,8 @@ class CreateTweets < ActiveRecord::Migration
       t.belongs_to :user
       t.datetime :datetime
       t.text :text
+      #t.integer :total_votes, default: 0
+      #t.integer :user_votes, default: 0
 
       t.timestamps
     end
