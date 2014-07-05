b3a275bee42fa53312346eaad5788e6ace99d598
Ben Brostoff & Sid Patel <pair+benbrostoff.sidpatel13@devbootcamp.com>
2014-07-05 14:16:12 -0400

    Better filtering for userpage tweet views - Sid and I are workign on how to order / sign in, sign out

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -13,6 +13,15 @@ post '/users/signin' do
 end
 
 get '/users/:id' do#all your tweets and how many votes you have
+  @tweets = []
+  @vote_array = []
+  Vote.where(user_id: current_user.id).each do |vote|
+    @vote_array << vote.current_votes #gives array of all votes
+    @tweets << Tweet.find(vote.tweet_id) #only tweets that were voted on
+  end
+  @vote_array = @vote_array.sort.reverse #ordered vote array
+  @tweets = @tweets.find_all{ |tweet| tweet.user_id == current_user.id }
+  #only tweets that are mine and tweets that got votes
   erb :user_page
 end
 
