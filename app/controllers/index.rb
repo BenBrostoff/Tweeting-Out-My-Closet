3e2b5740da58c147fdcc90a4d32a4af870aa7325
Ben Brostoff <ben.brostoff@gmail.com>
2014-07-06 19:02:19 -0400

    Solved initial display problem

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,5 @@
 get '/' do #this should be a log-in screen
-  @tweets = Tweet.all.order("average_votes DESC").order("user_votes DESC").take(5)
+  @tweets = Tweet.all.order("average_votes DESC").where.not(average_votes: 0).order("user_votes DESC").take(5)
   @high_scores = User.all.where.not(score: 0).order("score DESC").take(5)
   erb :index
 end
